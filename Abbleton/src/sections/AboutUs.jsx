4d182fa87db687406c6d6b5e475dca60a3b75095
AntoineD <dangleterre.antoine@gmail.com>
2024-03-28 11:29:31 +0100

    Fix background spacing

--- Abbleton/src/sections/AboutUs.jsx
+++ Abbleton/src/sections/AboutUs.jsx
@@ -9,9 +9,9 @@ function AboutUs() {
                 title={<>We make <span className='text-secondary'>Live</span>, <span className='text-secondary'>Push</span>, <span className='text-secondary'>Note</span> and <span className='text-secondary'>Link</span> — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</>}
                 subtext="Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world."
             />
-            <div className='relative w-full h-7/12'>
-                <div className='absolute right-0 flex justify-end w-7/12 h-full bg-[#FBFFA7]' />
-                <div className='relative flex items-center z-1 justify-evenly'>
+            <div className='relative w-full h-7/12 py-[10vw] lg:py-[8vw] flex items-center'>
+                <div className='absolute right-0 flex w-7/12 h-full bg-[#FBFFA7]' />
+                <div className='relative flex z-1 justify-evenly'>
                     <div className='max-w-[40%]'>
                         <img src={CompositionPicture} className='object-contain w-full h-full' alt="" />
                     </div>
--- Abbleton/src/sections/People.jsx
+++ Abbleton/src/sections/People.jsx
@@ -11,8 +11,8 @@ function People() {
                 title="We are more than 350 people from 30 different countries divided between our headquarters in Berlin and our offices in Los Angeles and Tokyo."
                 subtext="Most of us are active musicians, producers, and DJs, and many of us use Live and Push every day. We come from a wide range of cultural and professional backgrounds. Some of us have PhDs, some are self-taught, and most of us are somewhere in between. What connects us is the shared belief that each of us has the skills and knowledge to contribute to something big: helping to shape the future of music culture."
             />
-            <div className='relative flex items-center py-[10vw]'>
-                <div className='absolute left-0 flex justify-end w-7/12 h-full bg-[#B6FFC0]' />
+            <div className='relative flex items-center py-[10vw] lg:py-[8vw]'>
+                <div className='absolute left-0 flex w-7/12 h-full bg-[#B6FFC0]' />
                 <div className='relative z-50 flex items-center w-full justify-evenly'>
                     <div className='flex flex-col max-w-[30%]'>
                         <img src={DrawingPeople} className='object-contain w-full h-full' alt="" />
