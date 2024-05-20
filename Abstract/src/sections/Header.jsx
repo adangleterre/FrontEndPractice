979a846543a030c2af62b6a1ab8f530c9aa987b7
AntoineD <dangleterre.antoine@gmail.com>
2024-05-20 16:10:51 +0200

    Fix header spacing

--- Abstract/src/sections/Header.jsx
+++ Abstract/src/sections/Header.jsx
@@ -2,7 +2,7 @@ import brandLogo from '../assets/abstract-logo.svg'
 
 function Header() {
   return (
-    <div className="text-white bg-black h-[88px] flex items-center">
+    <div className="text-white bg-black h-[88px] flex items-center px-7">
       <div className="w-[90%] flex justify-between m-auto">
         <div className="flex items-center text-2xl leading-8">
           <img src={brandLogo} className='pr-3'/>
