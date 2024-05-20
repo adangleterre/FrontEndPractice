d2f9fdc6faf8c118333ea9ce87a21c861956bb04
AntoineD <dangleterre.antoine@gmail.com>
2024-05-20 16:04:10 +0200

    Add buttons to header

--- Abstract/src/sections/Header.jsx
+++ Abstract/src/sections/Header.jsx
@@ -8,6 +8,10 @@ function Header() {
           <img src={brandLogo} className='pr-3'/>
           <div className="pl-3 border-l-2 text-nowrap">Help center</div>
         </div>
+        <div>
+          <button className='px-6 py-2 ml-4 text-2xl border rounded-lg'>Submit a request</button>
+          <button className='ml-4 text-2xl bg-[#4C5FD4] hover:bg-white hover:text-black transition duration-300 px-8 py-2 rounded-lg'>Sign in</button>
+        </div>
       </div>
     </div>
   )
