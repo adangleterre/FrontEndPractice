8339427761725e2b79968845ab629b621aae8274
AntoineD <dangleterre.antoine@gmail.com>
2024-05-28 14:54:57 +0200

    Fix icon spacing on FaqTopic component

--- Abstract/src/components/FaqTopic.jsx
+++ Abstract/src/components/FaqTopic.jsx
@@ -1,8 +1,8 @@
 function FaqTopic({ iconSrc, topic, description }) {
     return (
         <div className="flex mb-12">
-            <div className="w-2/12 mr-5 sm:w-2/12 md:mr-8">
-                <img src={iconSrc} alt={topic + ` icon`} className="max-h-16 md:max-h-32"/>
+            <div className="mr-5">
+                <img src={iconSrc} alt={topic + ` icon`} className="max-h-16 md:max-h-24"/>
             </div>
             <div className="flex flex-col w-9/12 sm:w-10/12">
                 <h4 className="mb-4 text-2xl font-bold">{topic}</h4>
