3f2af6760800600c31ef9e2b08ac65892290ae09
AntoineD <dangleterre.antoine@gmail.com>
2024-05-30 10:09:57 +0200

    Add a component for footer categories

--- Abstract/src/sections/Footer.jsx
+++ Abstract/src/sections/Footer.jsx
@@ -1,30 +1,13 @@
 import minimalBrandLogo from '../assets/minimal-abstract-logo.svg'
+import FooterCategory from '../components/FooterCategory'
 
 function Footer() {
     return (
         <footer className="flex flex-col md:flex-row px-4 pt-12 pb-[60px] font-medium text-white bg-black lg:px-[5%]">
             <div className="flex flex-wrap justify-center w-full md:w-6/12 lg:w-8/12">
-                <div className="flex flex-col w-6/12 mb-12 lg:w-3/12">
-                    <h3 className="mb-4 text-2xl font-bold">Abstract</h3>
-                    <a href="#">Start Trial</a>
-                    <a href="#">Pricing</a>
-                    <a href="#">Download</a>
-                </div>
-                <div className="flex flex-col w-6/12 mb-12 lg:w-3/12">
-                    <h3 className="mb-4 text-2xl font-bold">Ressources</h3>
-                    <a href="#">Blog</a>
-                    <a href="#">Help Center</a>
-                    <a href="#">Release Notes</a>
-                    <a href="#">Status</a>
-                </div>
-                <div className="flex flex-col w-6/12 mb-12 lg:w-3/12">
-                    <h3 className="mb-4 text-2xl font-bold">Community</h3>
-                    <a href="#">Twitter</a>
-                    <a href="#">LinkedIn</a>
-                    <a href="#">Facebook</a>
-                    <a href="#">Dribbble</a>
-                    <a href="#">Podcast</a>
-                </div>
+                <FooterCategory Category="Abstract" links={["Start trial", "Pricing", "Download"]} />
+                <FooterCategory Category="Ressources" links={["Blog", "Help Center", "Release Notes", "Status"]} />
+                <FooterCategory Category="Community" links={["Twitter", "Linked", "Facebook", "Dribbble", "Podcast"]} />
                 <div className="flex flex-col w-6/12 mb-12 lg:w-3/12">
                     <h3 className="mb-4 text-2xl font-bold">Company</h3>
                     <a href="#">About Us</a>
