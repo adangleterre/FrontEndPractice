c37055cf72d04f26312d060220a6af861efc49da
AntoineD <dangleterre.antoine@gmail.com>
2024-06-07 15:17:41 +0200

    Add a component for header nav links

---
+++ Lobe-tour/src/components/HeaderLink.jsx
@@ -0,0 +1,7 @@
+function HeaderLink({linkText}) {
+  return (
+    <a href="#" className='hover:text-[#333] text-[#767676] transition hover:scale-105'>{linkText}</a>
+  )
+}
+
+export default HeaderLink
