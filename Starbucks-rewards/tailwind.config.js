2276613dce1cc9d4b0ba929efedc9fd89f3bf21a
AntoineD <dangleterre.antoine@gmail.com>
2024-06-19 10:25:57 +0200

    Add starbucks main color to tailwind theme

--- Starbucks-rewards/tailwind.config.js
+++ Starbucks-rewards/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "starbucks-green": "#01754A",
+      },
+    },
   },
   plugins: [],
 }
