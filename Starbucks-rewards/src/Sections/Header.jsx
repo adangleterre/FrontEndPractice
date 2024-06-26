b91d5aab58bbbf32e65fd54c1cf8875121f9aa62
AntoineD <dangleterre.antoine@gmail.com>
2024-06-26 10:38:50 +0200

    Add header

--- Starbucks-rewards/src/Sections/Header.jsx
+++ Starbucks-rewards/src/Sections/Header.jsx
@@ -1,4 +1,4 @@
-import Logo from '../assets/Starbucks_Logo.svg'
+import Logo from '/images/Starbucks_Logo.svg'
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoLocationSharp } from "react-icons/io5";
 
@@ -6,22 +6,22 @@ export function Header() {
     return (
         <>
             <header className="h-16 sm:h-[76px] md:h-20 xl:h-24">
-                <nav className="flex justify-between items-center h-full max-w-7xl mx-auto px-4">
+                <nav className="flex justify-between items-center h-full max-w-[1440px] mx-auto px-4">
                     <div className="flex items-center">
-                        <img src={Logo} className="max-h-11 top-2/4 right-full mr-6 lg:mr-10" alt="Logo starbucks"/>
-                        <ul className="hidden md:flex gap-5 text-xl font-bold">
+                        <img src={Logo} className="max-h-12 top-2/4 right-full mr-6 lg:mr-10" alt="Logo starbucks"/>
+                        <ul className="hidden md:flex gap-5 text-[15px] tracking-widest font-bold">
                             <li><a href="#" className="hover:text-starbucks-green">MENU</a></li>
-                            <li><a href="#" className="hover:text-starbucks-green">REWARDS</a></li>
+                            <li><a href="#" className="hover:text-starbucks-green border-b-[6px] border-starbucks-green pb-6 xl:pb-8">REWARDS</a></li>
                             <li><a href="#" className="hover:text-starbucks-green">GIFT CARDS</a></li>
                         </ul>
                     </div>
                     <div className="hidden md:flex flex-row items-center gap-5">
-                        <div className="flex flex-row items-center gap-2 font-bold hover:text-starbucks-green mr-10">
+                        <div className="flex flex-row items-center gap-2 font-semibold text-sm hover:text-starbucks-green mr-10">
                             <IoLocationSharp size={20}/>
                             <a href="#">Find a store</a>
                         </div>
-                        <button className="border-black">Sign in</button>
-                        <button className="border-black bg-black text-white">Join now</button>
+                        <button className="border-black font-semibold text-sm">Sign in</button>
+                        <button className="border-black bg-black text-white font-semibold text-sm">Join now</button>
                     </div>
 
                     <div className="md:hidden">
@@ -30,8 +30,8 @@ export function Header() {
                 </nav>
             </header>
             <div className="text-white bg-[#1F3832] flex flex-row items-center justify-between px-4 h-[50px] shadow-lg">
-                <p className="uppercase text-[13px] font-bold tracking-wider">Starbucks® rewards</p>
-                <button className="border-white text-white">Join the app</button>
+                <p className="uppercase text-[13px] md:text-sm font-bold tracking-wider md:pl-24 lg:pl-32 lg:pl-72">Starbucks® rewards</p>
+                <button className="border-white text-white md:hidden">Join the app</button>
             </div>
         </>
 
