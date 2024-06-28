7e89881af3735d3cf7cb6b49c9d57ca19a56520c
AntoineD <dangleterre.antoine@gmail.com>
2024-06-28 15:40:53 +0200

    Fix rewards banner and sticky position

--- Starbucks-rewards/src/Sections/Header.jsx
+++ Starbucks-rewards/src/Sections/Header.jsx
@@ -5,10 +5,10 @@ import { IoLocationSharp } from "react-icons/io5";
 export function Header() {
     return (
         <>
-            <header className="h-16 sm:h-[76px] md:h-20 xl:h-24">
+            <header className="h-[72px] md:h-20 xl:h-24">
                 <nav className="flex justify-between items-center h-full max-w-[1440px] mx-auto px-4">
                     <div className="flex items-center">
-                        <img src={Logo} className="max-h-12 top-2/4 right-full mr-6 lg:mr-10" alt="Logo starbucks"/>
+                        <img src={Logo} className="max-h-10 md:max-h-12 top-2/4 right-full mr-6 lg:mr-10" alt="Logo starbucks"/>
                         <ul className="hidden md:flex gap-5 text-[15px] tracking-widest font-bold">
                             <li><a href="#" className="hover:text-starbucks-green">MENU</a></li>
                             <li><a href="#" className="hover:text-starbucks-green border-b-[6px] border-starbucks-green pb-6 xl:pb-8">REWARDS</a></li>
@@ -29,7 +29,7 @@ export function Header() {
                     </div>
                 </nav>
             </header>
-            <div className="text-white bg-[#1F3832] flex flex-row items-center justify-between px-4 h-[50px] shadow-lg">
+            <div className="text-white bg-[#1F3832] flex flex-row items-center justify-between px-4 h-[50px] shadow-lg sticky top-0">
                 <p className="uppercase text-[13px] md:text-sm font-bold tracking-wider md:pl-24 lg:pl-32 lg:pl-72">Starbucks® rewards</p>
                 <button className="border-white text-white md:hidden">Join the app</button>
             </div>
