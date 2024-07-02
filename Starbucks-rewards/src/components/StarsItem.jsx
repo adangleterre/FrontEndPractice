bdf2a4ebfc54bddbb66e0c4e2ded349198cdcdf5
AntoineD <dangleterre.antoine@gmail.com>
2024-07-02 16:29:58 +0200

    Add responsive sizing for 'earn stars' section

---
+++ Starbucks-rewards/src/components/StarsItem.jsx
@@ -0,0 +1,13 @@
+function StarsItem({imgSource, title, description}) {
+    return (
+        <div className='flex gap-4 w-full md:w-6/12 lg:w-5/12 xl:w-4/12'>
+            <img src={imgSource} alt="#" width='112' className='h-full'/>
+            <div>
+                <h3 className='font-semibold'>{title}</h3>
+                <p className='pt-5'>{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default StarsItem;
