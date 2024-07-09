34dd99272954b5c0c854df3f48a783d89c04e35a
AntoineD <dangleterre.antoine@gmail.com>
2024-07-09 14:16:44 +0200

    Gave "a" element style responsability to components for more flexibility

--- Starbucks-rewards/src/components/RewardItem.jsx
+++ Starbucks-rewards/src/components/RewardItem.jsx
@@ -5,7 +5,7 @@ function RewardItem({imageSrc, linkText, description, href}) {
         <div className="flex md:flex-col items-center text-left md:text-center">
             <img src={imageSrc} width="190px" className="max-w-28 md:max-w-48 h-max" alt=""/>
             <p className="pl-4 md:pt-8">
-                <a href={href} className="inline-flex items-center">
+                <a href={href} className="inline-flex items-center text-[#006241] underline">
                     <span>{linkText}</span>
                     <GoLinkExternal className='mx-1'/>
                 </a>
