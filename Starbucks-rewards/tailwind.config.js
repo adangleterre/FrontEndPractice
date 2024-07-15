88747dd17a467fb080f01f0968dffd5f1ff89cd4
AntoineD <dangleterre.antoine@gmail.com>
2024-07-15 18:43:59 +0200

    Add shadcn accordion component

--- Starbucks-rewards/tailwind.config.js
+++ Starbucks-rewards/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
     extend: {
       colors: {
@@ -17,9 +21,22 @@ export default {
         'hero-mobile': "url('/images/backgrounds/hero-mobile.jpg')",
         'hero-desktop': "url('/images/backgrounds/hero-desktop.png')",
         'partners': "url('/images/backgrounds/bg-partners.webp')"
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
